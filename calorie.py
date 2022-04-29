2da7477f374d47cdf80fce7df475494f6ea8fc36
vonrimS <mr.mytab@gmail.com>
2022-04-29 12:17:02 +0200

    some scrapping

--- calorie.py
+++ calorie.py
@@ -1,13 +1,21 @@
+from temperature import Temperature
+
 class Calorie:
     """
-    Represent amount of calories calculated with
+    Represent amount of calories calculated for today with formula
     BMR = 10*weight + 6.25*height - 5*age + 5 - 10*temperature
     """
     def __init__(self, weight, height, age, temperature):
-        self.temperature = temperature
-        self.age = age
         self.height = height
         self.weight = weight
+        self.age = age
+        self.temperature = temperature
 
     def calculate(self):
-        pass
+        result = 10 * self.weight + 6.25 * self.height - 5 * self.age + 5 - 10 * self.temperature
+        return result
+
+if __name__ == '__main__':
+    temperature = Temperature(city="new-york", country='usa').get()
+    calorie = Calorie(weight=75, height=169, age=36, temperature=16)
+    print(calorie.calculate())
--- temperature.py
+++ temperature.py
@@ -1,11 +1,52 @@
+import requests
+from selectorlib import Extractor
+
+
 class Temperature:
     """
     Temperature scraped from weather webapp timeanddate.com/weather
     based on city and country user input
     """
+
+    headers = {
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    base_url = 'https://www.timeanddate.com/weather/'
+    yaml_path = 'temperature.yaml'
+
     def __init__(self,country, city):
-        self.city = city
-        self.country = country
+        self.country = country.replace(' ', '-')
+        self.city = city.replace(' ', '-')
+
+    def _build_url(self):
+        """builds the url string adding contry and city"""
+        url = self.base_url + self.country + '/' + self.city
+        return url
+
+    def _scrape(self):
+        """Extracts a value as instructed by the yaml file and returns a dictionary"""
+        url = self._build_url()
+        extractor = Extractor.from_yaml_file(self.yaml_path)
+        r = requests.get(url, headers=self.headers)
+        full_content = r.text
+        raw_content = extractor.extract(full_content)
+        return raw_content
 
     def get(self):
-        pass
+        """Cleans the output of _scrape"""
+        scraped_content = self._scrape()
+        return float(scraped_content['temp'].replace('°C', '').strip())
+
+# https://www.timeanddate.com/weather/usa/new-york
+
+print(__name__)
+if __name__ == '__main__':
+    temperature = Temperature(country='usa', city="new-york")
+    print(temperature.get())
